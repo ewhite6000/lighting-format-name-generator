b8b9e06f4c4c6cf46b79de7bd1f6faf1ba385b53
ewhite6000 <34250329+ewhite6000@users.noreply.github.com>
2021-03-28 20:53:52 -0500

    Initial implementation

---
+++ names.js
@@ -0,0 +1,3 @@
+var startNames = ['Global', 'Universal', 'Standardized', 'Superior', 'Amazing', 'Industry Wide', 'Advanced'];
+var middleNames = ['Profile', 'Control', 'Fixture', 'Fixture Profile', 'Device', 'Luminaire', 'Entertainment', 'Lighting'];
+var endNames = ['Format', 'Data Format', 'Protocol', 'Type Format', 'Standard', 'Description', 'Definition', 'System', 'Outline'];
