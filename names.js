f35839746143ff667151f9a2146d5954c88ee152
ewhite6000 <34250329+ewhite6000@users.noreply.github.com>
2021-03-28 21:01:53 -0500

    Add additional words

--- names.js
+++ names.js
@@ -1,3 +1,3 @@
-var startNames = ['Global', 'Universal', 'Standardized', 'Superior', 'Amazing', 'Industry Wide', 'Advanced'];
+var startNames = ['Global', 'Universal', 'Standardized', 'Superior', 'Systematic', 'Industry Wide', 'Advanced', 'Common', 'Comprehensive', 'Extensive', 'General', 'Widespread'];
 var middleNames = ['Profile', 'Control', 'Fixture', 'Fixture Profile', 'Device', 'Luminaire', 'Entertainment', 'Lighting'];
-var endNames = ['Format', 'Data Format', 'Protocol', 'Type Format', 'Standard', 'Description', 'Definition', 'System', 'Outline'];
+var endNames = ['Format', 'Data Format', 'Protocol', 'Type Format', 'Standard', 'Description', 'Definition', 'System', 'Scheme', 'Configuration', 'Structure'];
