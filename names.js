ca1b800f504930733c4ac7df7eb31eca42dc055e
Chris Love <chrisl@carallon.com>
2021-03-31 18:37:04 +0100

    Add ability to vary the number of middle words
    Tweak the names list to make this more natural

--- names.js
+++ names.js
@@ -1,3 +1,3 @@
-var startNames = ['Global', 'Universal', 'Standardized', 'Superior', 'Systematic', 'Industry Wide', 'Advanced', 'Common', 'Comprehensive', 'Extensive', 'General', 'Widespread'];
-var middleNames = ['Profile', 'Control', 'Fixture', 'Fixture Profile', 'Device', 'Luminaire', 'Entertainment', 'Lighting'];
-var endNames = ['Format', 'Data Format', 'Protocol', 'Type Format', 'Standard', 'Description', 'Definition', 'System', 'Scheme', 'Configuration', 'Structure'];
+var startNames = ['Global', 'Universal', 'Standardized', 'Superior', 'Systematic', 'Industry-Wide', 'Advanced', 'Common', 'Comprehensive', 'Extensive', 'General', 'Widespread'];
+var middleNames = ['Profile', 'Control', 'Fixture', 'Device', 'Luminaire', 'Entertainment', 'Lighting', 'Data', 'Type'];
+var endNames = ['Format', 'Protocol', 'Standard', 'Description', 'Definition', 'System', 'Scheme', 'Configuration', 'Structure'];
